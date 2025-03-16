029fd51d12dfee0759fc33e026fd95ecd1c9f9f1
rajib010 <pokhrelrajib016@gmail.com>
2025-03-16 14:11:46 +0545

    writing api for fetching videos and fixing public route error

---
+++ src/app/api/video/route.ts
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export async function GET(req: NextRequest) {
+  try {
+    const videos = await prisma.video.findMany({
+      orderBy: { createdAt: "desc" },
+    });
+
+    return NextResponse.json(videos);
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Error fetching videos" },
+      { status: 500 }
+    );
+  } finally {
+    await prisma.$disconnect();
+  }
+}
--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 
 const isPublicRoute = createRouteMatcher(["/sign-in", "/sign-up", "/", "/home"]);
 
-const isPublicApiRoutes = createRouteMatcher(["/api/videos"]);
+const isPublicApiRoutes = createRouteMatcher(["/api/video"]);
 
 export default clerkMiddleware(async (auth, req) => {
   const { userId } = await auth();
